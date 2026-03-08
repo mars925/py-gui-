99ebeb916ac3b6db0a7dfbb3d96441b3ffe67c44
mars925 <mars20120925@gmail.com>
2026-03-08 10:57:48 +0800

    新增 prj01.py 和 prj02.py 檔案，實作基本 GUI 功能與按鈕事件

--- prj02.py
+++ prj02.py
@@ -6,6 +6,11 @@ from tkinter import *
 #######################定義函數########################
 def hi_fun():
     print("Hi,singular")
+    display.config(text="Hi Singular", fg="red", bg="black")
+
+
+def clear_fun():
+    display.config(text="", fg="black", bg="white")
 
 
 #######################建立視窗########################
@@ -13,9 +18,11 @@ def hi_fun():
 windows = Tk()
 # 設定視窗標題
 windows.title("My First GUI")
-btn1 = Button(windows, text="click me", command=hi_fun)
+btn1 = Button(windows, text="show screen", command=hi_fun)
 btn1.pack()
-display = Label(windows, text="Hi Singular", fg="red", bg="black")
+btn2 = Button(windows, text="clear screen", command=clear_fun)
+btn2.pack()
+display = Label(windows, text="")
 display.pack()
 #######################運行應用程式########################
 # 運行應用程式
